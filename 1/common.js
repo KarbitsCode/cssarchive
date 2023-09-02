a555a9f9d776a2a56940b16d7be9ab8bcafb6098
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2023-09-02 21:52:20 +0800

    Git commit

---
+++ 1/common.js
@@ -0,0 +1,54 @@
+function autoRefresh(r,t) {
+  let redirect_url = r;
+  let delay = 1;
+  let input_time = document.getElementById("url_time");
+  let time = input_time.value = delay;
+  let min_time = Math.floor(t / 60000);
+  console.log("Auto refresh page in " + min_time + " minutes");
+  function redirect_page() {
+    if (time !== 1) {
+      time -= 1;
+      input_time.value = time;
+      setTimeout(redirect_page, 1000);
+    } else {
+      window.location.href = redirect_url;
+    };
+  };
+  setTimeout(redirect_page, t);
+};
+function gotoPage(l) {
+  if (window.document.documentMode) {
+    window.location.replace(l);
+  } else {
+    window.location.replace("../redir.html?go=" + l);
+  };
+};
+function keyEvent(k) {
+  window.onkeydown = function(event) {
+    if (event.ctrlKey && (event.which == k || event.keyCode == k)) {
+      console.log("Keys detected");
+      window.location.replace("#keys");
+      window.open("https://bit.ly/3p4mGEN");
+      window.location.replace("#main");
+    };
+  };
+};
+function adBlocker(u) {
+  console.log("AdBlocker detected");
+  setTimeout(function() {
+    window.location.replace("#adblock");
+    alert("AdBlocker Detected");
+    window.location.replace("#main");
+    window.location.replace(u);
+  }, 5000);
+};
+setTimeout(function() {
+  history.pushState("", document.title, window.location.pathname + "");
+  setInterval(function() {
+    if (document.visibilityState === "visible") {
+      document.title = "Arsip CSS";
+    } else {
+      document.title = "Main Page";
+    };
+  }, 100);
+}, 500);
