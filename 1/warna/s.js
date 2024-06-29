7745af92d2f4f0533360b81a54e5c68d4422477f
KarbitsCode <adipramana623@gmail.com>
2024-06-29 13:46:29 +0800

    Git commit

--- 1/warna/s.js
+++ 1/warna/s.js
@@ -1,8 +1,10 @@
 window.addEventListener("DOMContentLoaded", function(event) {
   document.querySelectorAll("a").forEach(function(element) {
     element.addEventListener("click", function(event) {
-      event.preventDefault();
-      window.location.replace(this.getAttribute("href"));
+      if (this.getAttribute("href") !== null) {
+        event.preventDefault();
+        window.location.replace(this.getAttribute("href"));
+      };
     });
   });
 });
